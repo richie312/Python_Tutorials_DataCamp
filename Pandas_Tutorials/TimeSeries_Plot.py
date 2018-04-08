ee64e8cf6d90714178c039293a0d735c31642187
richie312 <richie.chatterjee31@gmail.com>
2018-04-08 16:11:17 +0530

    added files

---
+++ Pandas_Tutorials/TimeSeries_Plot.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 15:45:18 2018
+
+@author: Richie
+"""
+
+## Visualise Time Seres pandas
+
+# Plot the raw data before setting the datetime index
+df.plot()
+plt.show()
+
+# Convert the 'Date' column into a collection of datetime objects: df.Date
+df.Date = pd.to_datetime(df['Date'])
+
+# Set the index to be the converted 'Date' column
+df.set_index('Date',inplace=True)
+
+# Re-plot the DataFrame to see that the axis is now datetime aware!
+df.plot()
+plt.show()
